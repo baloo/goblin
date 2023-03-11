1fc7ac2f466d6e63ad44482a1df63fea96f2f4b9
Raito Bezarius <masterancpp@gmail.com>
2023-03-11 17:02:03 +0100

    pe: implement write support for AttributeCertificate

--- src/pe/certificate_table.rs
+++ src/pe/certificate_table.rs
@@ -3,7 +3,7 @@
 /// https://learn.microsoft.com/en-us/windows/win32/debug/pe-format#the-attribute-certificate-table-image-only
 /// https://learn.microsoft.com/en-us/windows/win32/api/wintrust/ns-wintrust-win_certificate
 use crate::error;
-use scroll::Pread;
+use scroll::{ctx, Pread, Pwrite};
 
 use alloc::string::ToString;
 use alloc::vec::Vec;
@@ -38,7 +38,7 @@ impl TryFrom<u16> for AttributeCertificateRevision {
 }
 
 #[repr(u16)]
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub enum AttributeCertificateType {
     /// WIN_CERT_TYPE_X509
     X509 = 0x0001,
@@ -127,7 +127,28 @@ impl<'a> AttributeCertificate<'a> {
     }
 }
 
+impl<'a> ctx::TryIntoCtx<scroll::Endian> for &AttributeCertificate<'a> {
+    type Error = error::Error;
+
+    /// Writes an aligned attribute certificate in the buffer.
+    fn try_into_ctx(self, bytes: &mut [u8], ctx: scroll::Endian) -> Result<usize, Self::Error> {
+        let offset = &mut 0;
+        bytes.gwrite_with(self.length, offset, ctx)?;
+        bytes.gwrite_with(self.revision as u16, offset, ctx)?;
+        bytes.gwrite_with(self.certificate_type as u16, offset, ctx)?;
+        bytes.gwrite(self.certificate, offset)?;
+        // Extend by zero the buffer until it is aligned.
+        // TODO(RaitoBezarius): is there a better method to do this?
+        let aligned_offset = (*offset + 7) & !7;
+        let zero_buffer = vec![0u8; aligned_offset - *offset];
+        bytes.gwrite(&zero_buffer[..], offset)?;
+
+        Ok(*offset)
+    }
+}
+
 pub type CertificateDirectoryTable<'a> = Vec<AttributeCertificate<'a>>;
+
 pub(crate) fn enumerate_certificates(
     bytes: &[u8],
     table_virtual_address: u32,
